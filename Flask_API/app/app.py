3915be09113da22c07820b63fc2b044198f5c8ee
darespinoza <despinoza@uazuay.edu.ec>
2025-01-08 23:04:51 -0500

    [ADD] Postgres container and Flask DB conn

--- Flask_API/app/app.py
+++ Flask_API/app/app.py
@@ -1,9 +1,23 @@
-# app.py
+import os
+import psycopg2
 from flask import Flask, jsonify
 
 app = Flask(__name__)
 
-# Ruta GET para sena Time to Live
+# Conexion a base de datos
+def get_db_connection():
+    try:
+        conn = psycopg2.connect(
+            host=os.getenv('PG_HOST'),
+            database=os.getenv('PG_DB_NAME'),
+            user=os.getenv('PG_USER'),
+            password=os.getenv('PG_PASSWORD'),
+        )
+        return conn
+    except Exception as e:
+        raise
+
+# Ruta GET para senal Time to Live
 @app.route(
     '/ttl', 
     methods=['GET']
@@ -13,3 +27,21 @@ def ttl():
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
+
+# Ruta POST para testear conexion a base de datos
+@app.route(
+    '/db_check',
+    methods=['POST']
+)
+def db_check():
+    try:
+        conn = get_db_connection()
+        cursor = conn.cursor()
+        cursor.execute("SELECT version();")
+        results = cursor.fetchone()
+        cursor.close()
+        conn.close()
+
+        return f"Conectado a PostgreSQL v{results[0]}", 200
+    except Exception as e:
+        return f"Sin conexion a BD: {e}", 500
--- README.md
+++ README.md
@@ -12,17 +12,27 @@ http://localhost:8080/
 
 Paso hacia el Python API
 
+### Time to Alive (TTL)
+
 Ingresar en navegador a http://localhost:8080/api/ttl o usando el comando `curl`
 
 # Python API
 
-Testear el end-point
+Testear el end-point. Desde máquina local o de un contenedor que expone su puerto al exterior (máquina local)
 ```
-curl -X GET http://localhost:5555/ttl
+curl -X GET http://localhost:5000/ttl
 ```
 
 Comando curl con tipo de request, header y datos.
 
 ```
 curl -X POST -H "Content-Type: application/json" -d '{}' http://localhost:5000
+```
+
+# Comandos útiles Docker
+
+Verificar validez de la sintaxis y configuración del archivo `docker-compose.yml`
+
+```
+docker compose config
 ```
